cae33746198937ad6ba5d388a4a70fdd7f954597
Asher Hamrick <ashhamrick2@gmail.com>
2023-08-19 16:46:46 -0400

    update site

--- js/observer.js
+++ js/observer.js
@@ -1,19 +1,21 @@
-const faders = document.querySelectorAll('.fade');
+const faders = document.querySelectorAll(".fade");
 
 const appearOptions = {};
-console.log(faders);
-const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
-    entries.forEach(entry => {
-        if(!entry.isIntersecting){
-            return;
-        }
-        else{
-            entry.target.classList.add('appear');
-            appearOnScroll.unobserve(entry.target);
-        }
-    })
-}, appearOptions);
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
 
-faders.forEach(fader => {
-    appearOnScroll.observe(fader);
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
 });
